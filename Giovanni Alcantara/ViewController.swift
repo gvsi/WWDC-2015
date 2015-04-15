672e3722d385243d850df4319c43d7daca7d62dc
Giovanni Alcantara <me@giovannialcantara.com>
2015-04-15 09:51:53 +0100

    Initial Commit

---
+++ Giovanni Alcantara/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Giovanni Alcantara
+//
+//  Created by Giovanni Alcantara on 15/04/2015.
+//  Copyright (c) 2015 Giovanni Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
