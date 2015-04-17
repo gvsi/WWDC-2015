58b500a18aed665d72c28b0d66be0af45aae6c0a
Giovanni Alcantara <me@giovannialcantara.com>
2015-04-17 13:37:25 +0100

    Added Remove blur when clicking on logoButton, cleaned up code

--- Giovanni Alcantara/ViewController.swift
+++ Giovanni Alcantara/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var logoButton: LogoButton!
+    
+    func colorsWithHalfOpacity(colors: [CGColor]) -> [CGColor] {
+        return colors.map({ CGColorCreateCopyWithAlpha($0, CGColorGetAlpha($0) * 0.5) })
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        println(self.view.frame)
+        
+        logoButton.layer.addPulse { builder in
+            //builder.borderColors = [UIColor(hex: "34495e").CGColor]
+            builder.borderColors = [UIColor.blackColor().CGColor]
+            builder.backgroundColors = self.colorsWithHalfOpacity(builder.borderColors)
+            builder.path = UIBezierPath(ovalInRect: self.logoButton.bounds).CGPath
+            builder.duration *= 0.85
+            builder.repeatCount = Int.max
+            builder.lineWidth = 2.0;
+            builder.backgroundColors = []
+        }
+        
+        var size = max(self.view.frame.size.width, self.view.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +42,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func activate(sender: LogoButton!) {
-        sender.isActive = !sender.isActive
+        
+        self.logoButton.isActive = !self.logoButton.isActive
+        
+        //self.logoButton.isActive = !self.logoButton.isActive
+        let transitionOptions = UIViewAnimationOptions.TransitionCrossDissolve
+    
+        
+        UIView.animateWithDuration(2.0, delay: 0.2, options: transitionOptions, animations: {
+            self.backgroundImageView.alpha = 0.0
+            }, completion: { finished in
+        })
+        
+        UIView.animateWithDuration(2.0, delay: 0.0, options: transitionOptions, animations: {
+            self.logoButton.alpha = 0.0
+            }, completion: { finished in
+        })
+
     }
     
 }
